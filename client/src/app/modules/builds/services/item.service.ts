65166d7aaf97861a7249a27be533397f279989d9
Gilene <Matt.Gilene@us.sogeti.com>
2018-09-18 15:41:05 -0400

    Refactored ItemService to retry item fetch if it has previously failed

--- client/src/app/modules/builds/services/item.service.ts
+++ client/src/app/modules/builds/services/item.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Item, ApiRespones } from '../models/item';
 import { of, Observable, forkJoin } from 'rxjs';
-import { map, filter, mergeMap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 export interface ItemFilter {
   type?: string | string[];
@@ -16,38 +16,27 @@ export class ItemService {
   private items: Item[] = [];
 
   loading: boolean;
+  error: string;
 
-  constructor(private $http: HttpClient) {
-    this.loading = true;
+  constructor(private $http: HttpClient) {}
 
-    const getArmour = this.$http
-      .get<ApiRespones>('http://localhost:4300/armour')
-      .pipe(map(res => res.lines));
-    const getWeapons = this.$http
-      .get<ApiRespones>('http://localhost:4300/weapons')
-      .pipe(map(res => res.lines));
-    const getAccessories = this.$http
-      .get<ApiRespones>('http://localhost:4300/accessories')
-      .pipe(map(res => res.lines));
-    const getFlasks = this.$http
-      .get<ApiRespones>('http://localhost:4300/flasks')
-      .pipe(map(res => res.lines));
-    const getJewels = this.$http
-      .get<ApiRespones>('http://localhost:4300/jewels')
-      .pipe(map(res => res.lines));
+  getItems(): Observable<Item[]> {
+    if (this.items.length === 0) {
+      this.fetchItems().subscribe(
+        items => {
+          this.loading = false;
+          this.items = items;
+          this.error = '';
+        },
+        err => {
+          this.error =
+            'Could not fetch items from server, please refresh the page and try again.';
+          this.loading = false;
+        }
+      );
+    }
 
-    const request = forkJoin<Item[]>([
-      getArmour,
-      getWeapons,
-      getAccessories,
-      getFlasks,
-      getJewels
-    ]).pipe(map(responses => [].concat(...responses)));
-
-    request.subscribe(items => {
-      this.items = items;
-      this.loading = false;
-    });
+    return of(this.items);
   }
 
   getFilteredItems(itemFilter: ItemFilter): Observable<Item[]> | null {
@@ -55,7 +44,7 @@ export class ItemService {
       itemFilter.type = itemFilter.type.join();
     }
 
-    let filteredItems = of(this.items);
+    let filteredItems = this.getItems();
 
     if (itemFilter.type) {
       filteredItems = filteredItems.pipe(
@@ -80,5 +69,33 @@ export class ItemService {
 
   get isLoading() {
     return this.loading;
+  }
+
+  fetchItems(): Observable<Item[]> {
+    this.loading = true;
+
+    const getArmour = this.$http
+      .get<ApiRespones>('http://localhost:4300/armour')
+      .pipe(map(res => res.lines));
+    const getWeapons = this.$http
+      .get<ApiRespones>('http://localhost:4300/weapons')
+      .pipe(map(res => res.lines));
+    const getAccessories = this.$http
+      .get<ApiRespones>('http://localhost:4300/accessories')
+      .pipe(map(res => res.lines));
+    const getFlasks = this.$http
+      .get<ApiRespones>('http://localhost:4300/flasks')
+      .pipe(map(res => res.lines));
+    const getJewels = this.$http
+      .get<ApiRespones>('http://localhost:4300/jewels')
+      .pipe(map(res => res.lines));
+
+    return forkJoin<Item[]>([
+      getArmour,
+      getWeapons,
+      getAccessories,
+      getFlasks,
+      getJewels
+    ]).pipe(map(responses => [].concat(...responses)));
   }
 }
