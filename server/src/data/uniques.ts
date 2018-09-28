d4f70f5a8171e72faf9d5f2832ec2e526b3aebc6
Gilene <Matt.Gilene@us.sogeti.com>
2018-09-28 14:14:46 -0400

    Unique Item Data Parsing and API

--- server/src/data/uniques.ts
+++ server/src/data/uniques.ts
@@ -1,74 +1,32 @@
 import * as fs from "fs";
 import * as path from "path";
-import { Item } from "../../../exile-builds-core/models/Item";
+import axios from "axios";
 
 const uniquesPath = path.join(__dirname, "../../data/Uniques/");
 
-const filesToImport = fs.readdirSync(uniquesPath);
+const filesToImport = fs
+  .readdirSync(uniquesPath)
+  .filter(file => file.match(/.json$/));
 
-export const UniqueItemData = {
-  weapons: {},
-  armor: {},
-  accessories: {},
-  jewels: {},
-  flasks: {}
-};
-
-export function findUniques(query: Item | {}): Item[] {
-  const items: Item[] = [];
-
-  if (Object.keys(query).length === 0) {
-    Object.keys(UniqueItemData).forEach(key =>
-      items.push(...UniqueItemData[key])
-    );
-    return items;
-  }
-
-  for (let category of Object.keys(UniqueItemData)) {
-    for (let item of UniqueItemData[category]) {
-      console.log(item);
-      for (let param of Object.keys(query)) {
-        if (item[param].match(query[param])) {
-          console.log("found item", item.name);
-          items.push(item);
-        } else {
-          continue;
-        }
-      }
-    }
-  }
-
-  return items;
-}
+export const UniqueItemData: any = {};
 
 for (let file of filesToImport) {
   loadUniques(file).then(contents => {
     for (let key of Object.keys(contents.default)) {
-      if (key.match(/Weapon/g)) {
-        UniqueItemData.weapons = Object.assign(
-          contents.default[key],
-          UniqueItemData.weapons
-        );
-      } else if (key.match(/Jewel/g)) {
-        UniqueItemData.jewels = Object.assign(
-          contents.default[key],
-          UniqueItemData.jewels
-        );
-      } else if (key.match(/(Shield|Quiver|Helmet|Gloves|Boots|Body)/g)) {
-        UniqueItemData.armor = Object.assign(
-          contents.default[key],
-          UniqueItemData.armor
-        );
-      } else if (key.match(/Amulet|Belt|Ring/g)) {
-        UniqueItemData.accessories = Object.assign(
-          contents.default[key],
-          UniqueItemData.accessories
-        );
-      } else if (key.match(/Flask/g)) {
-        UniqueItemData.flasks = Object.assign(
-          contents.default[key],
-          UniqueItemData.flasks
-        );
+      const parts = key.split(":");
+      const slot = parts[0].trim();
+      if (
+        slot !== "Quiver" &&
+        slot !== "Ring" &&
+        slot !== "Amulet" &&
+        slot !== "Belt"
+      ) {
+        const type = parts[1].trim();
+        if (!UniqueItemData[slot]) UniqueItemData[slot] = {};
+        UniqueItemData[slot][type] = contents.default[key];
+      } else {
+        if (!UniqueItemData[slot]) UniqueItemData[slot] = {};
+        UniqueItemData[slot] = contents.default[key];
       }
     }
   });
