42c29c440d26257d95fdcdbd8066c8c79f541cb1
Gilene <Matt.Gilene@us.sogeti.com>
2018-10-04 09:36:22 -0400

    Item API Caching

--- server/src/routes/itemApi.ts
+++ server/src/routes/itemApi.ts
@@ -4,5 +4,6 @@ import { UniqueItemData } from "../data/uniques";
 export const itemApi = Router();
 
 itemApi.get("/uniques", (req: Request, res: Response) => {
+  res.setHeader("Cache-Control", "public, max-age=86400"); // Cache for 1 day
   return res.json(UniqueItemData);
 });
